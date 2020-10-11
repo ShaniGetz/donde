cfbdf382276a2977beda5540ff46502c07f40bb6
sheba77 <50077384+sheba77@users.noreply.github.com>
2020-10-11 15:08:42 +0300

    be4 merge

--- app/src/main/java/com/example/donde/utils/OfflineDataTransfer.java
+++ app/src/main/java/com/example/donde/utils/OfflineDataTransfer.java
@@ -1,41 +1,15 @@
 package com.example.donde.utils;
 
-
-import android.Manifest;
 import android.content.Context;
-import android.content.pm.PackageManager;
-import android.location.Address;
-import android.location.Geocoder;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-
-import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Parcel;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.nearby.Nearby;
 import com.google.android.gms.nearby.connection.AdvertisingOptions;
 import com.google.android.gms.nearby.connection.ConnectionInfo;
 import com.google.android.gms.nearby.connection.ConnectionLifecycleCallback;
 import com.google.android.gms.nearby.connection.ConnectionResolution;
 import com.google.android.gms.nearby.connection.ConnectionsClient;
+import com.google.android.gms.nearby.connection.ConnectionsStatusCodes;
 import com.google.android.gms.nearby.connection.DiscoveredEndpointInfo;
 import com.google.android.gms.nearby.connection.DiscoveryOptions;
 import com.google.android.gms.nearby.connection.EndpointDiscoveryCallback;
@@ -44,105 +18,214 @@ import com.google.android.gms.nearby.connection.PayloadCallback;
 import com.google.android.gms.nearby.connection.PayloadTransferUpdate;
 import com.google.android.gms.nearby.connection.PayloadTransferUpdate.Status;
 import com.google.android.gms.nearby.connection.Strategy;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
+
+import com.google.firebase.firestore.GeoPoint;
+
 import java.io.IOException;
+import java.util.Hashtable;
 import java.util.List;
 import java.util.Locale;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class OfflineDataTransfer extends AppCompatActivity {
-    private static final int REQUEST_CODE_REQUIRED_PERMISSIONS = 1;
-    private static final Strategy STRATEGY = Strategy.P2P_STAR;
+public class OfflineDataTransfer{
+    String myName;
+    GeoPoint geoPoint;
+    String myStatus;
+    Context context;
+    String EndpointId;
+    String SERVICE_ID;
+    Hashtable<String, GeoPoint> Location_dict = new Hashtable<String, GeoPoint>();
+    Hashtable<String, String> Status_dict = new Hashtable<String, String>();
 
-    private static final String[] REQUIRED_PERMISSIONS =
-            new String[]{
-                    Manifest.permission.BLUETOOTH,
-                    Manifest.permission.BLUETOOTH_ADMIN,
-                    Manifest.permission.ACCESS_WIFI_STATE,
-                    Manifest.permission.CHANGE_WIFI_STATE,
-            };
-
-    private ConnectionsClient connectionsClient;
-    private final String codeName = CodenameGenerator.generate();
-
-    private Location myLocation = null;
-    private Location opponentLocation = null;
-
-    private String opponentEndpointId;
-    private String opponentName;
+    private static final Strategy STRATEGY = Strategy.P2P_CLUSTER;
 
 
-    // Callbacks for receiving payloads for getting other location
-    private final PayloadCallback payloadCallback = new PayloadCallback() {
-        @Override
-        public void onPayloadReceived(String endpointId, Payload payload) {
-            //create an empty parcel
-            Parcel parcel = Parcel.obtain();
-            //add byte from payload to empty parcel
-            parcel.unmarshall(payload.asBytes(), 0, payload.asBytes().length);
-            //something important
-            parcel.setDataPosition(0);
-            //get location
-            opponentLocation = Location.CREATOR.createFromParcel(parcel);
-//            otherLocationView.setText(opponentLocation.getLongitude() + " " + opponentLocation.getLatitude());
+    OfflineDataTransfer(String name, GeoPoint geopoint, Context Context, String status){
+        myName = name;
+        geoPoint = geopoint;
+        context = Context;
+        myStatus = status;
+        SERVICE_ID = context.getPackageName();
 
-        }
-        @Override
-        public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
-        }
-    };
+    }
 
-    // Callbacks for finding other devices
-    private final EndpointDiscoveryCallback endpointDiscoveryCallback =
-            new EndpointDiscoveryCallback() {
-                @Override
-                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
-                    Log.i("TAG", "onEndpointFound: endpoint found, connecting");
-                    connectionsClient.requestConnection(codeName, endpointId, connectionLifecycleCallback);
-                }
+    public void updateLocation(GeoPoint geopoint){
+        geoPoint = geopoint;
+    }
 
-                @Override
-                public void onEndpointLost(String endpointId) {
-                }
-            };
+    public void updateStatus(String status){
+        myStatus = status;
+    }
+    public GeoPoint getOtherLocation(String name){
+        return Location_dict.get(name);
 
+    }
 
+    public String getOtherStatus(String name){
+        return Status_dict.get(name);
 
-    // Callbacks for connections to other devices
-    private final ConnectionLifecycleCallback connectionLifecycleCallback =
+    }
+
+    private ConnectionLifecycleCallback connectionLifecycleCallback =
             new ConnectionLifecycleCallback() {
                 @Override
-                public void onConnectionInitiated(String endpointId, ConnectionInfo connectionInfo) {
-                    Log.i("TAG", "onConnectionInitiated: accepting connection");
-                    connectionsClient.acceptConnection(endpointId, payloadCallback);
-                    opponentName = connectionInfo.getEndpointName();
+                public void onConnectionInitiated(@NonNull String endpointId, @NonNull ConnectionInfo connectionInfo) {
+                    // Automatically accept the connection on both sides.
+                    Nearby.getConnectionsClient(context).acceptConnection(endpointId, payloadCallback);
+                    EndpointId = endpointId;
+                    Log.d("TAG","Connection initiated : " + endpointId + " ," + connectionInfo.toString());
                 }
 
                 @Override
                 public void onConnectionResult(String endpointId, ConnectionResolution result) {
-                    if (result.getStatus().isSuccess()) {
-                        Log.i("TAG", "onConnectionResult: connection successful");
-
-                        connectionsClient.stopDiscovery();
-                        connectionsClient.stopAdvertising();
-
-//                        opponentEndpointId = endpointId;
-//                        setOpponentName(opponentName);
-//                        setStatusText(getString(R.string.status_connected));
-//                    } else {
-//                        Log.i(TAG, "onConnectionResult: connection failed");
+                    switch (result.getStatus().getStatusCode()) {
+                        case ConnectionsStatusCodes.STATUS_OK:
+                            // We're connected! Can now start sending and receiving data.
+                            Log.d("TAG","Connected : " + endpointId + " ," + result.toString());
+//                            Nearby.getConnectionsClient(context).stopAdvertising();
+                            break;
+                        case ConnectionsStatusCodes.STATUS_CONNECTION_REJECTED:
+                            // The connection was rejected by one or both sides.
+                            Log.d("TAG","Connection rejected : " + endpointId + " ," + result.toString());
+                            break;
+                        case ConnectionsStatusCodes.STATUS_ERROR:
+                            // The connection broke before it was able to be accepted.
+                            break;
+                        default:
+                            // Unknown status code
                     }
+
                 }
-//
+
                 @Override
                 public void onDisconnected(String endpointId) {
-                    Log.i("TAG", "onDisconnected: disconnected from the opponent");
-//                    resetGame();
+                    // We've been disconnected from this endpoint. No more data can be
+                    // sent or received.
                 }
             };
 
+    private PayloadCallback payloadCallback =
+            new PayloadCallback() {
+                @Override
+                public void onPayloadReceived(String endpointId, Payload payload) {
+                    // This always gets the full data of the payload. Will be null if it's not a BYTES
+                    // payload. You can check the payload type with payload.getType().
+                    byte[] receivedBytes = payload.asBytes();
+                }
 
+                @Override
+                public void onPayloadTransferUpdate(String endpointId, PayloadTransferUpdate update) {
+                    // Bytes payloads are sent as a single chunk, so you'll receive a SUCCESS update immediately
+                    // after the call to onPayloadReceived().
+                }
+            };
+
+    private EndpointDiscoveryCallback endpointDiscoveryCallback =
+            new EndpointDiscoveryCallback() {
+                @Override
+                public void onEndpointFound(String endpointId, DiscoveredEndpointInfo info) {
+                    // An endpoint was found. We request a connection to it.
+                    Log.d("TAG", "Endpoint found : " + info.getEndpointName());
+                    getNameAndStatus(info.getEndpointName());
+                    Nearby.getConnectionsClient(context)
+                            .requestConnection(info.getEndpointName() + ".1", endpointId, connectionLifecycleCallback)
+                            .addOnSuccessListener(
+                                    (Void unused) -> {
+                                        Log.d("TAG", "Request a connection :" + info.getEndpointName());
+                                        // We successfully requested a connection. Now both sides
+                                        // must accept before the connection is established.
+                                    })
+                            .addOnFailureListener(
+                                    (Exception e) -> {
+                                        // Nearby Connections failed to request the connection.
+                                        Log.d("TAG", "Nearby Connections failed to request the connection.");
+                                    });
+                }
+
+                @Override
+                public void onEndpointLost(String endpointId) {
+                    // A previously discovered endpoint has gone away.
+                }
+            };
+
+    public void startAdvertising() {
+        AdvertisingOptions advertisingOptions =
+                new AdvertisingOptions.Builder().setStrategy(STRATEGY).build();
+        Nearby.getConnectionsClient(context)
+                .startAdvertising(
+                        getUser(), SERVICE_ID, connectionLifecycleCallback, advertisingOptions)
+                .addOnSuccessListener(
+                        (Void unused) -> {
+                            // We're advertising!
+                            Log.d("TAG", "Advertising : " + getUser());
+
+                        })
+                .addOnFailureListener(
+                        (Exception e) -> {
+                            // We were unable to start advertising.
+                            Log.d("TAG", "Unable to start advertising");
+                        });
+    }
+
+    public void startDiscovery() {
+        DiscoveryOptions discoveryOptions = new DiscoveryOptions.Builder().setStrategy(STRATEGY).build();
+        Nearby.getConnectionsClient(context)
+                .startDiscovery(SERVICE_ID, endpointDiscoveryCallback, discoveryOptions)
+                .addOnSuccessListener(
+                        (Void unused) -> {
+                            // We're discovering!
+                            Log.d("TAG", "Discovering...");
+                        })
+                .addOnFailureListener(
+                        (Exception e) -> {
+                            // We're unable to start discovering.
+                            Log.d("TAG", "We're unable to start discovering");
+                        });
+
+    }
+
+
+    private void stopAdvertising() {
+        Nearby.getConnectionsClient(context).stopAdvertising();
+    }
+
+    public String getUser(){
+        return myName + "#" + myStatus + "#" + geoPoint.getLatitude() + "#" + geoPoint.getLongitude();
+    }
+
+    public String getNameAndStatus(String string){
+        boolean foundName = false;
+        boolean foundStat = false;
+        boolean foundLatitude = false;
+        String Name ="";
+        String stat ="";
+
+        int idx = 0;
+        String StringOfLatitude;
+        GeoPoint geoPoint;
+
+        for(int i =0; i < string.length(); i++) {
+            if (string.charAt(i) == '#') {
+                if (!foundName) {
+                    foundName = true;
+                    Name = string.substring(0, i);
+
+                } else if (!foundStat) {
+                    stat = string.substring(0, i);
+                    idx = i;
+                    foundStat = true;
+                    Status_dict.put(Name, stat);
+                } else if (!foundLatitude) {
+                    StringOfLatitude = string.substring(idx, i);
+                    foundLatitude = true;
+                    geoPoint = new GeoPoint(Double.parseDouble(StringOfLatitude),
+                            Double.parseDouble(string.substring(i)));
+                    Location_dict.put(Name, geoPoint);
+
+                }
+            }
+        }
+        return Name;
+    }
 }
