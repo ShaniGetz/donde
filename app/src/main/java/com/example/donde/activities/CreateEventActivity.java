802047a0d52ff1b13d19f8ff4c2839d15de93d42
sheba77 <50077384+sheba77@users.noreply.github.com>
2020-10-12 14:59:07 +0300

    time not working

--- app/src/main/java/com/example/donde/activities/CreateEventActivity.java
+++ app/src/main/java/com/example/donde/activities/CreateEventActivity.java
@@ -462,19 +462,21 @@ public class CreateEventActivity extends AppCompatActivity implements OnMapReady
         textViewEventTime = findViewById(R.id.create_textView_event_time);
         Date currDate = Calendar.getInstance().getTime();
         textViewEventTime.setText(currDate.toString());
+        int hour = 10;
+        int minute = 60;
 
         textViewEventTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String mcurrentTime = textViewEventTime.getText().toString();
-                int hour = -1;
-                int minute = -1;
-                for(int i = 0; i < mcurrentTime.length(); i++){
-                    if(mcurrentTime.charAt(i) == ':'){
-                        hour = Integer.parseInt(mcurrentTime.substring((i-3), (i-1)));
-                        minute = Integer.parseInt(mcurrentTime.substring((i+1), (i+3)));
-                    }
-                }
+//                String mcurrentTime = textViewEventTime.getText().toString();
+//                int hour = -1;
+//                int minute = -1;
+//                for(int i = 0; i < mcurrentTime.length(); i++){
+//                    if(mcurrentTime.charAt(i) == ':'){
+//                        hour = Integer.parseInt(mcurrentTime.substring((i-3), (i-1)));
+//                        minute = Integer.parseInt(mcurrentTime.substring((i+1), (i+3)));
+//                    }
+//                }
                 TimePickerDialog mTimePicker;
 //                int resTheme = R.style.SpinnerTimePicker;
                 int resTheme =
